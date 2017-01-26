b5d030dc01eee02d4e940167dd17eddcf0f39926
Cameron Bennett <2193356B@student.gla.ac.uk>
2017-01-26 22:14:15 +0000

    chapter4

--- first_tango_proj/settings.py
+++ first_tango_proj/settings.py
@@ -12,8 +12,22 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATIC_DIR= os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +70,7 @@ ROOT_URLCONF = 'first_tango_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
--- first_tango_proj/urls.py
+++ first_tango_proj/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,7 +26,9 @@ from django.contrib import admin
 from django.conf.urls import include
 from rango import views
 urlpatterns = [
+    url(r'^$' , views.about, name='about'),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
+    url(r'^rango/about/', include('rango.urls')),   
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
