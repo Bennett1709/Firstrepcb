729e5d688944a2c6223fe62d2f026b12631e0707
Cameron Bennett <2193356B@student.gla.ac.uk>
2017-02-07 16:53:06 +0000

    end of chapter 7

--- first_tango_proj/urls.py
+++ first_tango_proj/urls.py
@@ -26,9 +26,7 @@ from django.contrib import admin
 from django.conf.urls import include
 from rango import views
 urlpatterns = [
-    url(r'^$' , views.about, name='about'),
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
-    url(r'^rango/about/', include('rango.urls')),   
     url(r'^admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
